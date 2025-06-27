b39cf0566104aa938ce9b6f2fe24ead65024fd71
MohamedYenge <mohamedyenge999@gmail.com>
2025-06-27 10:42:53 -0400

    final project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -28,7 +28,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e)
   };
 const handleCheckoutShopping = (e) => {
   alert('Functionality to be added for future reference');
